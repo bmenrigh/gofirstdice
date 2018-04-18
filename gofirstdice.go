38321d354ab6510d20ce2c9a21977923e4603787
Brandon Enright <bmenrigh@brandonenright.net>
2018-04-18 21:03:16 +0000

    Now able to compute min/max tally sum left

--- gofirstdice.go
+++ gofirstdice.go
@@ -15,11 +15,17 @@ const (
 )
 
 var (
+	PERMGOAL uint64
+	TALLYGOAL uint64
+)
+
+var (
 	dlet = [10]string{`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`} // 10 should be enough for anyone
 )
 
 type diceset [DICE][SIDES]uint8
-type placetally [DICE * SIDES][DICE]uint64
+type placetally [DICE * SIDES][DICE]uint64 // [value] then [place]
+type tallyleft [DICE][SIDES][DICE]uint64   // [row] then [column] then [place]
 
 
 // Sorting of permutations
@@ -31,8 +37,13 @@ func (a SortPerms) Less(i, j int) bool {if len(a[i]) == len(a[j]) {return a[i] <
 
 func main() {
 
-	tally_cache := build_tally_table()
-	fmt.Printf("tally 0 place 0: %d\n", tally_cache[0][0])
+	PERMGOAL = intpow(SIDES, DICE) / factorial(DICE)
+	TALLYGOAL = intpow(SIDES, DICE) / uint64(DICE)
+
+	tally_table := build_tally_table()
+	var min_tally, max_tally tallyleft
+
+	fmt.Printf("tally 0 place 0: %d\n", tally_table[0][0])
 
 	var dset diceset
 
@@ -41,6 +52,8 @@ func main() {
 			dset[d][s] = (uint8(s) * DICE) + uint8(d)
 		}
 	}
+
+	find_tally_left(&dset, 0, &tally_table, &min_tally, &max_tally)
 
 	fmt.Printf("Got str: %s\n", strings.Join(setstring(dset), ``))
 
@@ -208,4 +221,45 @@ func build_tally_table() placetally {
 	}
 
 	return tally_cache
+}
+
+
+func find_tally_left(dset *diceset, row uint8, tally_table *placetally,
+	min_tally *tallyleft, max_tally *tallyleft) {
+
+	l := SIDES // The limit
+
+	for p := uint8(0); p < DICE; p++ { // for each place
+		for s := l; s >= 1; s-- { // start from last to first
+			min := intpow(SIDES, DICE)
+			max := uint64(0)
+
+			for d := row; d < DICE; d++ { // for each dice you could pick
+				v := (*dset)[d][s - 1]; // The value
+
+				t := tally_table[v][p]
+
+				if (t < min) {
+					min = t
+				}
+				if (t > max) {
+					max = t
+				}
+			} // end for d
+
+			//fprintf(stderr, "Adding min,max for s=%d; p=%d to %lu, %lu\n", s, p, min, max);*/
+			//fprintf(stderr, "at r=%d; s=%d; p=%d; min=%lu; max=%lu\n", rows, s, p, min, max);*/
+
+			// If this is the last column it doesn't get added to the next higher column
+			if (s == l) {
+				(*min_tally)[row][s - 1][p] = min;
+				(*max_tally)[row][s - 1][p] = max;
+			} else {
+				(*min_tally)[row][s - 1][p] = min + (*min_tally)[row][s][p];
+				(*max_tally)[row][s - 1][p] = max + (*max_tally)[row][s][p];
+			}
+			// fmt.Printf("Bringing min,max for s=%d; p=%d to %d, %d\n", s, p, (*min_tally)[row][s - 1][p], (*max_tally)[row][s - 1][p]);
+
+		} // end for s
+	}
 }
